e608ddcaa95590c47bd5aa795f5bde7bd20bf8b6
YiHsuan <v61265@gmail.com>
2022-05-20 16:20:53 +0800

    refactor(/form/qa/button): change onClick name and adjust style of disable

--- packages/qa/src/react-components/form/buttons.js
+++ packages/qa/src/react-components/form/buttons.js
@@ -11,32 +11,27 @@ const ButtonWrapper = styled.button`
   font-size: 18px;
   line-height: 150%;
   color: #ffffff;
-  background: #04295e;
   border-radius: 6px;
   border: 0;
-  &:hover {
-    cursor: pointer;
-    background: #000928;
-  }
   ${(props) =>
-    props.diabled &&
-    `
-  background: #E0E0E0;
-  &:hover {
-    cursor: auto;
-    background: #E0E0E0;
-  }
-  `}
+    props.diabled
+      ? `
+  background: #E0E0E0;`
+      : `
+      background: #04295e;
+      &:hover {
+        cursor: pointer;
+        background: #000928;
+      }
+      `}
 `;
 
-export default function Button({ title, handleOnClick, disabled }) {
+export default function Button({ title, onClick, disabled }) {
+  function handleOnClick(e) {
+    if (!disabled) onClick(e);
+  }
   return (
-    <ButtonWrapper
-      onClick={(e) => {
-        if (!disabled) handleOnClick(e);
-      }}
-      diabled={disabled}
-    >
+    <ButtonWrapper onClick={handleOnClick} diabled={disabled}>
       {title}
     </ButtonWrapper>
   );
--- packages/qa/src/react-components/index.js
+++ packages/qa/src/react-components/index.js
@@ -3,21 +3,17 @@ import Checkbox from "./form/checkbox";
 import Button from "./form/buttons";
 
 export default function QA(props) {
-  function handleOnClick(e) {
+  function onClick(e) {
     console.log("click", e);
   }
   return (
     <>
       <Checkbox title='你是否有以下經歷？（複選）' />
       <div>
-        <Button
-          title='開始查詢'
-          handleOnClick={handleOnClick}
-          disabled={false}
-        />
+        <Button title='開始查詢' onClick={onClick} disabled={false} />
       </div>
 
-      <Button title='我不能按' handleOnClick={handleOnClick} disabled={true} />
+      <Button title='我不能按' onClick={onClick} disabled={true} />
     </>
   );
 }
