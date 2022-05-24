02083621c07064060d94b17248c02ba76e3b3a88
DianYangFu <dianyangfu@gmail.com>
2022-05-24 16:53:11 +0800

    refactor(qa/form): render component `button` in `question.js`

--- packages/qa/src/react-components/form/buttons.js
+++ packages/qa/src/react-components/form/buttons.js
@@ -5,7 +5,7 @@ const ButtonWrapper = styled.button`
   width: 100%;
   max-width: 320px;
   padding: 12px;
-  font-family: "Noto Sans CJK TC";
+  font-family: "Noto Sans CJK TC", sans-serif;
   font-style: normal;
   font-weight: 400;
   font-size: 18px;
@@ -14,7 +14,7 @@ const ButtonWrapper = styled.button`
   border-radius: 6px;
   border: 0;
   ${(props) =>
-    props.diabled
+    props.disabled
       ? `
   background: #E0E0E0;`
       : `
@@ -31,7 +31,7 @@ export default function Button({ title, onClick, disabled }) {
     if (!disabled) onClick(e);
   }
   return (
-    <ButtonWrapper onClick={handleOnClick} diabled={disabled}>
+    <ButtonWrapper onClick={handleOnClick} disabled={disabled}>
       {title}
     </ButtonWrapper>
   );
