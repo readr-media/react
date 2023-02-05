4f2ff63617b8ce83994c1111ceca723c738cad03
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-02-06 01:48:38 +0800

    feat(related-post): add typedef.js

---
+++ packages/related-post/src/typedef.js
@@ -0,0 +1,11 @@
+export default {}
+
+/**
+ *  @typedef {Object}            Post
+ *  @property {number | string}  id
+ *  @property {string}           caption
+ *  @property {string}           [slug]
+ *  @property {string}           [imageUrl]
+ *  @property {string}           [alt]
+ *  @property {string}           [link]
+ */
