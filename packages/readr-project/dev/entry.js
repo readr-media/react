a1edb68b5e461ead92e0b3c7b4db42cd078f3419
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-02-16 12:24:07 +0800

    feat(readr-project): remove `ShareButton`

--- packages/readr-project/dev/entry.js
+++ packages/readr-project/dev/entry.js
@@ -1,13 +1,7 @@
 import React from 'react' // eslint-disable-line
 import { createRoot } from 'react-dom/client'
 import styled from 'styled-components'
-import {
-  ReadrLogo,
-  DonateButton,
-  SubscribeButton,
-  RelatedPost,
-  ShareButton,
-} from '../src'
+import { ReadrLogo, DonateButton, SubscribeButton, RelatedPost } from '../src'
 
 const reactRootId = 'root'
 const container = document.getElementById(reactRootId)
@@ -36,24 +30,63 @@ const data = [
     date: '2023/1/2',
     time: 10,
   },
+  {
+    id: 2,
+    slug: 'post-slug-01',
+    caption: '社群平臺上的俄烏戰爭：美國也有責任？臺灣最在意中國威脅？',
+    imageUrl:
+      'https://www.readr.tw/assets/images/cl14pnaz4000e11vwb4ay1qv5-mobile.png',
+    alt: '文章敘述01',
+    link: 'https://www.mirrormedia.mg',
+    date: '2023/1/2',
+    time: 10,
+  },
+  {
+    id: 3,
+    slug: 'post-slug-01',
+    caption: '社群平臺上的俄烏戰爭：美國也有責任？臺灣最在意中國威脅？',
+    imageUrl:
+      'https://www.readr.tw/assets/images/cl14pnaz4000e11vwb4ay1qv5-mobile.png',
+    alt: '文章敘述01',
+    link: 'https://www.mirrormedia.mg',
+    date: '2023/1/2',
+    time: 10,
+  },
+  {
+    id: 4,
+    slug: 'post-slug-01',
+    caption: '社群平臺上的俄烏戰爭：美國也有責任？臺灣最在意中國威脅？',
+    imageUrl:
+      'https://www.readr.tw/assets/images/cl14pnaz4000e11vwb4ay1qv5-mobile.png',
+    alt: '文章敘述01',
+    link: 'https://www.mirrormedia.mg',
+    date: '2023/1/2',
+    time: 10,
+  },
 ]
+
+const ClickFunction = () => {
+  console.log('click button')
+}
 
 root.render(
   <>
     <Header>
       <ReadrLogo
-        width={50}
-        color=""
+        width="80px"
         className="logo"
         onClick={() => console.log('click logo')}
       />
-      <ShareButton color="black" />
     </Header>
-    <SubscribeButton className="test" backgroundColor="#04295e" />
-    <DonateButton className="test2" />
+
+    <div>
+      <SubscribeButton className="subscribe-test" onClick={ClickFunction} />
+    </div>
+
+    <DonateButton className="donate-test" />
+
     <Report>
       <RelatedPost relatedData={data} title="最新資訊" highlightColor="white" />
-      <RelatedPost relatedData={data} />
     </Report>
   </>
 )
--- packages/readr-project/src/index.js
+++ packages/readr-project/src/index.js
@@ -2,6 +2,5 @@ import ReadrLogo from './readr-logo'
 import SubscribeButton from './subscribe-button'
 import DonateButton from './donate-button'
 import RelatedPost from './related-post'
-import ShareButton from './share-button'
 
-export { ReadrLogo, DonateButton, SubscribeButton, RelatedPost, ShareButton }
+export { ReadrLogo, DonateButton, SubscribeButton, RelatedPost }
