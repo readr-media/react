afff41ec5181e8ae42c248ca7216545b03f9731f
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-02-19 18:45:31 +0800

    feat(mirrormedia-project): add `mirror-media-logo` components

---
+++ packages/mirrormedia-project/src/mirror-media-logo/index.js
@@ -0,0 +1,46 @@
+import React from 'react' // eslint-disable-line
+import styled from 'styled-components'
+import { MirrorMediaIcon } from './react-components/icon'
+
+const Logo = styled.a`
+  display: inline-block;
+  width: 63px;
+  height: auto;
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: 90.7px;
+  }
+`
+
+/**
+ * @param {Object} props
+ * @param {string} [props.color]
+ * - color of logo.
+ * - optional, default value is `''`.
+ * @param {string} [props.className]
+ * - className of logo.
+ * - optional, default value is `'mirrormedia-logo'`.
+ * @param {import("react").MouseEventHandler} [props.onClick]
+ * - logo onClick function.
+ * - optional, default value is {()=> void}.
+ * @returns {JSX.Element}
+ */
+
+export default function MirrorMediaLogo({
+  className = 'mirrormedia-logo',
+  color = '',
+  onClick,
+}) {
+  return (
+    <Logo
+      href="https://www.mirrormedia.mg"
+      target="_blank"
+      rel="noreferrer noopenner"
+      className={className}
+      onClick={onClick}
+    >
+      <MirrorMediaIcon color={color} />
+    </Logo>
+  )
+}
