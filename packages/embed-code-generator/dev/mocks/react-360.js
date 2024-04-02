5fea68b73e46e144cc90d4cd31787f9fe44c4a88
caesar <caesar.lee@mirrormedia.mg>
2024-04-02 15:01:07 +0800

    refactor(ecg): update react-360 version
    - update react-360 mock data

--- packages/embed-code-generator/dev/mocks/react-360.js
+++ packages/embed-code-generator/dev/mocks/react-360.js
@@ -5,15 +5,20 @@ export default {
     mb:
       'https://v3-statics-dev.mirrormedia.mg/images/548526e4-428a-49ff-b994-719c1e833f95-w2400.webP',
   },
-  hotspotsConfig: JSON.stringify([
-    {
-      type: 'info',
-      pitch: 0,
-      yaw: 0,
-      text: 'this is a hotspot',
-      url: 'https://www.google.com',
-    },
-  ]),
+  config: {
+    hotspots: [
+      {
+        type: 'info',
+        pitch: 0,
+        yaw: 0,
+        text: 'this is a hotspot',
+        url: 'https://www.google.com',
+      },
+    ],
+    pitch: -6,
+    yaw: -90,
+    showControls: true,
+  },
   isFullScreenWidth: false,
   caption: 'Add caption here.',
 }
